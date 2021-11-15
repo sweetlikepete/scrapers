ab03a90185675c92988f78309beac22179934892
Peter Bodechtel <sweetlikepete@gmail.com>
2021-11-15 11:00:18 +0200

    tweaks to reduce error output

--- src/entities/itunes/get-albums.ts
+++ src/entities/itunes/get-albums.ts
@@ -2,7 +2,6 @@
 
 import path from "path";
 
-import cheerio from "cheerio";
 import cliProgress from "cli-progress";
 import colors from "colors";
 import db from "better-sqlite3";
@@ -41,13 +40,14 @@ interface ITunesSearchResponse{
 
 
 const limit = 200;
-const requestDelay = 500; // itunes api limits you to 20 calls per minute, which is 3000 ms
+const requestDelay = 500; // Itunes api limits you to 20 calls per minute, which is 3000 ms
 const artistNamesDatabasePath = path.join(process.cwd(), "data/allmusic.com", "artist-names.db");
 const artistNamesDatabaseExists = await fs.pathExists(artistNamesDatabasePath);
 const outputDirectory = path.join(process.cwd(), "data/itunes");
 const outputIndexLength = 3;
-const completedDatabasePath = path.join(outputDirectory, "completed-artist-names.db")
+const completedDatabasePath = path.join(outputDirectory, "completed-artist-names.db");
 
+await fs.ensureDir(outputDirectory);
 
 const completedArtistNamesDatabase = db(completedDatabasePath);
 
@@ -62,7 +62,7 @@ if(!artistNamesDatabaseExists){
 
 }
 
-const completedArtists = completedArtistNamesDatabase.prepare("SELECT * FROM artists").all()
+const completedArtists = completedArtistNamesDatabase.prepare("SELECT * FROM artists").all();
 
 const completedArtistNames = completedArtistNamesDatabase.prepare("SELECT * FROM artists").all()
 .map((item: { name: string }) => item.name)
@@ -90,7 +90,7 @@ for(const artistName of artistNames){
     if(completedArtistNames.includes(artistName)){
 
         total += completedArtists.find((item) => item.name === artistName).completes;
-    
+
         bar.increment(1, { globalTotal: total });
 
     }else{
@@ -99,20 +99,19 @@ for(const artistName of artistNames){
         const albumSearchUrl = `https://itunes.apple.com/search?term=${ artist.toLocaleLowerCase() }&limit=${ limit }&entity=album`;
         const [response, error, cached] = await fetchJSON<ITunesSearchResponse>(albumSearchUrl);
         const completes: string[] = [];
-    
+
         if(error){
-    
+
             console.log(` Error downloading ${ albumSearchUrl }`);
-    
+
         }else{
-    
+
             const albums = response!.results.filter((item) => item.collectionType === "Album");
-            const hasError = false;
-    
+
             if(albums.length > 0){
-    
+
                 const batchCompletes = await Promise.all(albums.map(async (album): Promise<string> => {
-    
+
                     const id = String(album.artistId);
                     const outputFolder = path.join(outputDirectory, `${ id.slice(0, outputIndexLength) }`);
                     const outputImage = path.join(outputFolder, `${ id }.jpg`);
@@ -120,66 +119,68 @@ for(const artistName of artistNames){
                     const imageExists = await fs.pathExists(outputImage);
                     const jsonExists = await fs.pathExists(outputJSON);
                     const imageUrl = album.artworkUrl100.replace("source/100x100", "source/500x500");
-    
+
                     if(!jsonExists){
-    
+
                         await fs.ensureDir(outputFolder);
-    
+
                         try{
-    
+
                             await fs.writeFile(outputJSON, JSON.stringify(album));
-    
-                        }catch(error){
-                            
+
+                        }catch{
+
                             console.log(` Error writing json: ${ outputJSON }`);
-    
+
                         }
-    
+
                     }
-    
+
                     if(!imageExists){
-    
+
                         try{
-    
+
                             await downloadImage(imageUrl, outputImage);
-    
+
                         }catch(error){
-                            
+
+                            console.log(error);
+
                             console.log(` Error writing image: ${ imageUrl }`);
-    
+
                         }
-    
+
                     }
-    
+
                     return String(album.artistId);
-    
+
                 }));
-    
+
                 completes.push(...batchCompletes);
-    
+
             }
-    
+
             const query = `INSERT or REPLACE INTO artists(name, completes) VALUES ('${ artistName }', ${ completes.length })`;
-    
+
             completedArtistNamesDatabase.exec(query);
-        
+
             total += completes.length;
 
             bar.increment(1, { globalTotal: total });
-    
+
             if(!cached){
-        
+
                 await new Promise((resolve) => {
-        
+
                     setTimeout(resolve, requestDelay);
-            
+
                 });
-            
+
             }
 
         }
 
-    
+
     }
 
 }
--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,7 +1,6 @@
 
 
 import path from "path";
-import client from "https";
 
 import fetch from "node-fetch";
 import fs from "fs-extra";
@@ -11,7 +10,7 @@ import Downloader from "nodejs-file-downloader";
 const cacheDirectory = path.join(process.cwd(), "cache/fetch");
 const fetchOptions = {
     headers: {
-        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
         "accept-encoding": "gzip, deflate, br",
         "accept-language": "en-US,en;q=0.9,de;q=0.8,pt;q=0.7,af;q=0.6",
         "sec-ch-ua": "\"Google Chrome\";v=\"95\", \"Chromium\";v=\"95\", \";Not A Brand\";v=\"99\"",
@@ -23,9 +22,9 @@ const fetchOptions = {
         "sec-fetch-user": "?1",
         "upgrade-insecure-requests": "1",
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"
-        
+
     }
-}
+};
 
 
 await fs.ensureDir(cacheDirectory);
@@ -44,7 +43,7 @@ const errorResponse = (error: unknown): [undefined, Error, boolean] => {
 };
 
 
-export const fetchText = async (url: string, retries=3): Promise<[string, undefined, boolean] | [undefined, Error, boolean]> => {
+export const fetchText = async (url: string, retries = 3): Promise<[string, undefined, boolean] | [undefined, Error, boolean]> => {
 
     const cachePath = path.join(cacheDirectory, url);
     const exists = await fs.pathExists(cachePath);
@@ -57,11 +56,10 @@ export const fetchText = async (url: string, retries=3): Promise<[string, undefi
 
             return [cache.toString(), undefined, true];
 
-        }else{
-
-            await fs.rm(cachePath);
-
         }
+
+        await fs.rm(cachePath);
+
 
     }
 
@@ -77,9 +75,9 @@ export const fetchText = async (url: string, retries=3): Promise<[string, undefi
             await fs.writeFile(cachePath, body);
 
             return [body, undefined, false];
-    
+
         }
-    
+
         return [undefined, new Error(`Blank response from fetchText: ${ url }`), false];
 
     }catch(error: unknown){
@@ -136,14 +134,29 @@ export const fetchJSON = async <ResponseType>(url: string): Promise<[ResponseTyp
 
 
 export const downloadImage = async (url: string, filepath: string): Promise<void> => {
-    
-    const downloader = new Downloader({     
-        url,     
-        directory: path.dirname(filepath), 
+
+    await fs.ensureDir(path.dirname(filepath));
+
+    const downloader = new Downloader({
+        cloneFiles: false,
+        directory: path.dirname(filepath),
         fileName: path.basename(filepath),
-        maxAttempts:3
-    }) 
-    
-    await downloader.download();
+        maxAttempts: 3,
+        url
+    });
+
+    try{
+
+        await downloader.download();
+
+    }catch(error){
+
+        if(!["unlink", "rename"].includes((error as { syscall: string }).syscall)){
+
+            throw error;
+
+        }
+
+    }
 
 };
